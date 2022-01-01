f0403e3c8a160c38cd464a1a88c9b088357431e7
forevercynical <94206142+forevercynical@users.noreply.github.com>
2021-12-31 17:19:02 -0800

    Update helpers.py
    updated dev gen

--- proudcatowner/utils/helpers.py
+++ proudcatowner/utils/helpers.py
@@ -7,7 +7,7 @@ import requests
 
 def generate_device_info() -> dict:
     return {
-        "device_id": "32E3DDD1C3607790DE1D2F4AECD1C6E81D3F49EB2B93DC1203984EE5B83EAEA89B80A2D32978ABFD1A",
+        "device_id": device.deviceGenerator(),
         "user_agent": "Dalvik/2.1.0 (Linux; U; Android 7.1.2; SM-G965N Build/star2ltexx-user 7.1.; com.narvii.amino.master/3.4.33592)"
     }
 
